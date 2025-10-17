4b936284471359809ff7a51f2d895d60b99e1fb3
Pradeep Awasthi <pawasthi063@gmail.com>
2025-10-17 14:14:43 +0530

    Hooks useState

--- vite-basics/src/App.jsx
+++ vite-basics/src/App.jsx
@@ -3,13 +3,54 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
 function App() {
-  const [count, setCount] = useState(0)
+  
+  //   const [count, setCount] = useState(0)
+
+  // return (
+  //   <>
+  //     <div>
+  //       <a href="https://vite.dev" target="_blank">
+  //         <img src={viteLogo} className="logo" alt="Vite logo" />
+  //       </a>
+  //       <a href="https://react.dev" target="_blank">
+  //         <img src={reactLogo} className="logo react" alt="React logo" />
+  //       </a>
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => setCount((count) => count + 1)}>
+  //         count is {count}
+  //       </button>
+  //       <p>
+  //         Edit <code>src/App.jsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </>
+  // )
+  
+  let [count, setCount] = useState(0);
+  const addValue=()=>{
+    if(count<20){
+      count++;
+    }
+    setCount(count)
+
+  }
+  const removeValue=()=>{
+    if(count>0){
+      count--;
+    }
+    setCount(count)
+
+  }
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,7 +69,14 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */} 
+
+    <h1> Lets Understand the hooks with a Counter</h1>
+    <h2> Counter Value: {count}</h2>
+    <button onClick={addValue
+    }>Add a Value</button>
+    <br/>
+    <button onClick={removeValue}> Remove a Value </button>
     </>
   )
 }
--- vite-basics/src/main.jsx
+++ vite-basics/src/main.jsx
@@ -15,10 +15,10 @@ const reactElement = React.createElement(// built in react custom
   'Click me to visit Google'
 )
 createRoot(document.getElementById('root')).render(
-  reactElement,
-  anotherElement,
-  //  <>
-  //      <App />
-  //      <New />
-  //    </>
+  // reactElement,
+  // anotherElement,
+   <>
+       <App />
+       <New />
+     </>
 )
